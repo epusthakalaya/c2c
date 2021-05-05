f19987ff51b6df5b40e45b0ecf693572bdb323ae
epusthakalaya <81864477+epusthakalaya@users.noreply.github.com>
2021-05-05 13:49:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,13 +24,13 @@ except:
 @datgbot.on(events.NewMessage(pattern="/start"))
 async def _(event):
     ok = await datgbot(GetFullUserRequest(event.sender_id))
-    await event.reply(f"𝙷𝚎 𝚑𝚎`{ok.user.first_name}`!\n\n𝙸 𝚊𝚖 𝚊 𝙲𝚑𝚊𝚗𝚗𝚕𝚎 𝙿𝚘𝚜𝚝𝚎𝚛 𝚋𝚘𝚝. \n 𝙿𝚛𝚎𝚜𝚜 /help 𝚝𝚘 𝚐𝚎𝚝 𝚒𝚗𝚏𝚘 \nI can be used in only two channels (one user) at a time..\n[🤘](https://telegra.ph/file/1eca514b5e6202b1d92b3.jpg)", 
-    buttons = [[Button.url("🤝Main Group🛰️", url="t.me/danuma01"), Button.url("📝Bot News🛸", url="https://t.me/Dbotai")],[Button.url("✈️Developer✈️", url="https://lasiya.ml"),Button.url("🤙 Contact Dev 🛶", url="https://t.me/Danuma_admin_bot")]],link_preview=True)
+    await event.reply(f"𝙷𝚎 𝚑𝚎`{ok.user.first_name}`!\n\n𝙸 𝚊𝚖 𝚊 Telegtam 𝙲𝚑𝚊𝚗𝚗e𝚕 𝙿𝚘𝚜𝚝𝚎𝚛 𝚋𝚘𝚝 🇱🇰. \n 𝙿𝚛𝚎𝚜𝚜 /help 𝚝𝚘 𝚐𝚎𝚝 𝚒𝚗𝚏𝚘 \nI can be used in only two channels (one user) at a time..\n[🤘](https://telegra.ph/file/52a900e99c879218bd65f.jpg)", 
+    buttons = [[Button.url("Channel🛸", url="https://t.me/epusthakalaya_bots")],[Button.url("✈️Developer✈️", url="https://t.me/Madushanka_bro"),]],link_preview=True)
 
 
 @datgbot.on(events.NewMessage(pattern="/help"))
 async def helpp(event):
-    await event.reply("**Help**\n\nThis bot will send all new posts in one channel to the other channel (without forwarded tag).\nIt can be used only in two channels at a time, \n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!\n\nLiked the bot? Drop a ♥Danuma projectject")
+    await event.reply("**Help**\n\nThis bot will send all new posts in one channel to the other channel (without forwarded tag).\nIt can be used only in two channels at a time, \n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!\n\nPowered by E PUSTHAKALAYA BOTs™")
 
 @datgbot.on(events.NewMessage(incoming=True, chats=frm)) 
 async def _(event): 
@@ -42,5 +42,5 @@ async def _(event):
 
 
 print("Bot has started.")
-print("Visit @Danuma01")
+print("Visit @epusthakalaya_bots")
 datgbot.run_until_disconnected()
